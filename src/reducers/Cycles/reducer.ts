c11f1eb6c0ad0b421d87dcc11450e9b189194b1c
RosieCarvalho <rosie.baltazar.carvalho@gmail.com>
2024-01-12 17:06:14 -0300

    uso do immer

--- src/reducers/Cycles/reducer.ts
+++ src/reducers/Cycles/reducer.ts
@@ -22,6 +22,12 @@ export function cyclesReducer(state: CyclesState, action: any) {
         cycles: [...state.cycles, action.payload.newCycle],
         activeCycleId: action.payload.newCycle.id,
       };
+    // COM IMMER:
+    //  return produce(state, (draft) => {
+    //   draft.cycles.push(action.payload.newCycle)
+    //   draft.activeCycleId = action.payload.newCycle.id
+    // })
+
     case ActionTypes.INTERRUPT_CURRENT_CYCLE:
       return {
         ...state,
@@ -34,6 +40,19 @@ export function cyclesReducer(state: CyclesState, action: any) {
         }),
         activeCycleId: null,
       };
+    //COM IMMER
+    //   const currentCycleIndex = state.cycles.findIndex((cycle) => {
+    //     return cycle.id === state.activeCycleId
+    //   })
+
+    //   if (currentCycleIndex < 0) {
+    //     return state
+    //   }
+    //   return produce(state, (draft) => {
+    //     draft.activeCycleId = null
+    //     draft.cycles[currentCycleIndex].interruptedDate = new Date()
+    //   })
+    // }
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
       return {
         ...state,
@@ -46,6 +65,11 @@ export function cyclesReducer(state: CyclesState, action: any) {
         }),
         activeCycleId: null,
       };
+    //   return produce(state, (draft) => {
+    //     draft.activeCycleId = null
+    //     draft.cycles[currentCycleIndex].finishedDate = new Date()
+    //   })
+    // }
     default:
       return state;
   }

3a6be11dab3b9547e228fcc7b1a419eea0bc3522
RosieCarvalho <rosie.baltazar.carvalho@gmail.com>
2024-01-12 17:24:09 -0300

    add localstorage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -20,7 +20,7 @@ export function Countdown() {
       setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate
+          new Date(activeCycle.startDate)
         );
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished();
